20148fa4252206d34485ad227cd18237990dde91
ken20001207 <varedx@gmail.com>
2019-11-13 16:45:48 +0800

    f

--- js/main.js
+++ js/main.js
@@ -1,14 +1,19 @@
 // 初始化
 var status = 0;
 $.backstretch("images/landingpage.jpg");
-$(".form").fadeOut(1);
+$(".form").hide();
+$(".smallButtonArea").hide();
 
 function buttonclick() {
 
     if (status == 0) {
+
         status = 1;
         $.backstretch("images/form.jpg");
-        $(".form").fadeIn(3000);
+        $(".form").show();
+        $(".bigButtonArea").hide();
+        $(".smallButtonArea").show();
+
     } else if (status == 1) {
 
         if ($("#input_name").val() == "" || $("#input_contact").val() == "") {
@@ -25,7 +30,8 @@ function buttonclick() {
             },
             success: function(data) {
                 if (data == 1) {
-                    $(".form").fadeOut(1);
+                    $(".form").hide();
+                    $(".smallButtonArea").hide();
                     $.backstretch("images/suc.jpg");
                 } else {
                     alert("报名失败，请联系勤创工作人员。");
